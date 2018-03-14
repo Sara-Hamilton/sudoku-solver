a787b4ba574e328916fc31dc516f441979c3ed74
Sara <sara@indeterminate.net>
2018-03-13 20:53:42 -0700

    modiy verify array function with passing tests

--- js/verifyarray.js
+++ js/verifyarray.js
@@ -1,18 +1,16 @@
-// import { Square } from './../js/square.js';
-
-// export function Sudoku(){
-//
-// }
-
-
 export function VerifyArray(testArray) {
   console.log(testArray);
   testArray.sort();
   console.log(testArray);
   var goodArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  if (sortedTestArray === goodArray) {
-    return "true";
-  } else {
+  if (testArray.length !== goodArray.length) {
     return "false";
   }
-};
+  for ( var i = 0; i < goodArray.length; i++) {
+    if ( testArray[i] !== goodArray[i]) {
+      return "false";
+    } else {
+      return "true";
+    }
+  }
+}
--- spec/verifyarray_spec.js
+++ spec/verifyarray_spec.js
@@ -1,14 +1,22 @@
-// import { Sudoku } from './../js/sudoku.js';
-// import { Square } from './../js/square.js';
 import { VerifyArray } from './../js/verifyarray.js';
 
 describe('VerifyArray', function() {
 
-  it("will verify if array contains 1-9", function() {
+  it("will verify if array does not contain 1-9", function() {
     var testArray = [1,2,6,7,8,8,9,3,0];
     var result = VerifyArray(testArray);
-    console.log(result);
+    expect(result).toEqual("false");
+  });
 
+  it("will verify if array does contain 1-9", function() {
+    var testArray = [1,2,9,8,7,6,3,4,5];
+    var result = VerifyArray(testArray);
+    expect(result).toEqual("true");
+  });
+
+  it("will verify if array length is not 9", function() {
+    var testArray = [1,2,9,8,7,6,3,4];
+    var result = VerifyArray(testArray);
     expect(result).toEqual("false");
   });
 
